bee41070138f9723d51129d5504fed379ad1c010
Shankhadeep Biswas <sankhadeep.genx@gmail.com>
2020-10-01 21:57:39 +0530

    Initial commit - Dynamic weather app

---
+++ server.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+const fetch = require('node-fetch')
+const port = process.env.PORT || 3000
+    //fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${process.env.API}`)
+  
