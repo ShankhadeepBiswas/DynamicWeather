bc100dabb18daa5869ab064504d69925c54b4adc
Shankhadeep Biswas <sankhadeep.genx@gmail.com>
2020-10-10 15:25:03 +0530

    Checking the requests module , doing API calls, logging results on console

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 require('dotenv').config();
-const fetch = require('node-fetch')
+const requests = require('requests')
 const port = process.env.PORT || 3000
     //fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${process.env.API}`)
-  
+    const main=(req,res)=>{
+        if(req.url=='/'){
+            requests(`https://api.openweathermap.org/data/2.5/weather?q=Kolkata&units=metric&appid=${process.env.API}`, { streaming : true })
+            .on('data', function (chunk) {
+                const jsonData = JSON.parse(chunk)
+                console.log(jsonData.main.temp)
+            })
+            .on('end', function (err) {
+                if (err) return console.log('connection closed due to errors', err);
+            });
+        }
+    }
+    const server = require('http').createServer(main).listen(port,null,null,()=>{
+        console.log("App running on port",port);
+    })
