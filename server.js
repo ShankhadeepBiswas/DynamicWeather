c856d9fcb3e0d5b9dd77f1fc9052e30f361dd68d
Shankhadeep Biswas <sankhadeep.genx@gmail.com>
2020-10-13 14:17:47 +0530

    Almost done with the app

--- server.js
+++ server.js
@@ -4,22 +4,25 @@ var html = fs.readFileSync('home.html','utf-8')
 const requests = require('requests')
 const port = process.env.PORT || 3000
     //fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${process.env.API}`)
-    const replaceVal =(fake,{main,name,sys})=>{
-        let temp = fake.replace('{%tempval%}',main.temp)
-        temp = fake.replace('{%tempmin%}',main.temp_min)
-        temp = fake.replace('{%tempmax%}',main.temp_max)
-        temp = fake.replace('{%loc%}',name)
-        temp = fake.replace('{%country%}',sys.country)
-        temp = fake.replace('{%tempstatus%}',weather[0].main)
+    const replaceVal =(fake,{main,name,sys,weather})=>{
+        let temp = fake.replace(/{%tempval%}/,main.temp)
+        temp = temp.replace(/{%tempmin%}/,main.temp_min)
+        temp = temp.replace(/{%tempmax%}/,main.temp_max)
+        temp = temp.replace(/{%loc%}/,name)
+        temp = temp.replace(/{%country%}/,sys.country)
+        temp = temp.replace(/{%tempstatus%}/,weather[0].main)
         return temp
     }
     const main=(req,res)=>{
         if(req.url=='/'){
-            requests(`https://api.openweathermap.org/data/2.5/weather?q=Kolkata&units=metric&appid=${process.env.API}`, { streaming : true })
+            var loc;
+            prompt('Enter the name of any city',loc);
+            requests(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&units=metric&appid=${process.env.API}`, { streaming : true })
             .on('data', function (chunk) {
                 const jsonData = JSON.parse(chunk)
-                replaceVal(html,jsonData)
-                console.log(jsonData)
+                // console.log(replaceVal(html,jsonData));
+                res.write(replaceVal(html,jsonData))
+                res.end();
             })
             .on('end', function (err) {
                 if (err) return console.log('connection closed due to errors', err);
