72388b405cb9a165a685e9a0385775c2549acae7
Shankhadeep Biswas <sankhadeep.genx@gmail.com>
2020-10-14 13:20:47 +0530

    Dynamic weather of Kolkata Complete

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 require('dotenv').config();
+const prompt = require('prompt')
+const schema ={
+    properties: {
+        name: {
+          pattern: /^[a-zA-Z\s\-]+$/,
+          message: 'It must be a city name',
+          required: true
+        }
+      }
+}
 const fs = require('fs')
 var html = fs.readFileSync('home.html','utf-8')
 const requests = require('requests')
 const port = process.env.PORT || 3000
-    //fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${process.env.API}`)
     const replaceVal =(fake,{main,name,sys,weather})=>{
         let temp = fake.replace(/{%tempval%}/,main.temp)
         temp = temp.replace(/{%tempmin%}/,main.temp_min)
@@ -11,13 +20,19 @@ const port = process.env.PORT || 3000
         temp = temp.replace(/{%loc%}/,name)
         temp = temp.replace(/{%country%}/,sys.country)
         temp = temp.replace(/{%tempstatus%}/,weather[0].main)
+        temp = temp.replace(/{%weather%}/,weather[0].description)
         return temp
     }
     const main=(req,res)=>{
         if(req.url=='/'){
-            var loc;
-            prompt('Enter the name of any city',loc);
-            requests(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&units=metric&appid=${process.env.API}`, { streaming : true })
+//             var loc;
+//             prompt.start();
+//             prompt.get(schema, function (err, result) {
+//                 loc = result.name
+//              console.log('Command-line input received:');
+//              console.log('city-name:' + result.name);
+//   })
+            requests(`https://api.openweathermap.org/data/2.5/weather?q=Kolkata&units=metric&appid=${process.env.API}`, { streaming : true })
             .on('data', function (chunk) {
                 const jsonData = JSON.parse(chunk)
                 // console.log(replaceVal(html,jsonData));
